95847a8c9b5c950afd8f5daa05df8457bd73c0a8
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2021-04-11 21:05:45 +0200

    Project setup

---
+++ lib/region/generic/molecules/app_title.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTitle extends StatelessWidget {
+  const AppTitle({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Title.
+        Text(
+          "The Feed's Book",
+          style: GoogleFonts.goudyBookletter1911(
+            fontSize: 42,
+            fontWeight: FontWeight.w900
+          ),
+        ),
+
+        // Sub title.
+        Text(
+          "Read your feeds in a classic manor using your Surface Duo.",
+          style: GoogleFonts.goudyBookletter1911(
+            textStyle: Theme.of(context).textTheme.subtitle1,
+            fontSize: 21,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ],
+    );
+  }
+}
